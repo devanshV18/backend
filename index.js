5f207be8e2e684fd0bfa783a7adce7c3f7805210
devanshV18 <devanshverma024@gmail.com>
2024-02-19 20:28:58 +0530

    comments in index.js

--- index.js
+++ index.js
@@ -61,5 +61,5 @@ app.get('/github',(req,res) => {
 })
 
 app.listen(process.env.PORT, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`) //logs in terminal as confirmation of listening or running the server
 })
